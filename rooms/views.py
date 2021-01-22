25b29f786e415b06dd11da0f4d3eb14f078789b7
yeonju <apple2062@naver.com>
2021-01-23 02:06:20 +0900

    after using "Paginator" !! so cooool

--- rooms/views.py
+++ rooms/views.py
@@ -9,29 +9,13 @@ from . import models
 
 
 def all_rooms(request):
-    page = request.GET.get(
-        "page", 1
-    )  # url 에서 온 page 가 문자열이고 내가 1로 설정한 default 는 숫자이므로 에러 방지위해 int형변환을 해주는것임
-    page = int(  # url 에서 온 page 가 문자열이고 내가 1로 설정한 default 는 숫자이므로 에러 방지위해 int형변환을 해주는것임
-        page or 1
-    )  # 이미 위에서 1로 페이지가 생성됐다면, 다시 빈페이지로 page 입력 시, 에러나 나기 떄문에 이를 방지 하기 위함
-    page_size = 10
-    limit = page_size * page
-    offset = limit - page_size
-    all_rooms = models.Room.objects.all()[offset:limit]
-    page_count = ceil(models.Room.objects.count() / page_size)
-    # render 의 인자를 살펴보면, request 가 필요함.
-    # request 없이 response 는 있을 수 없음. 그래서 뭔가를 render 하고 싶다면, request를 주어야 함.(render(request, ... ))
-    return render(
-        request,
-        "rooms/home.html",
-        context={
-            "rooms": all_rooms,
-            "page_num": page,
-            "page_count": page_count,
-            "page_range": range(1, page_count),
-        },
-    )  # context 에 지정한 변수명과 변수를 담아 해당 template으로 전달
-    # template 에서는 받을 변수를 {{}} 안에 호출시킨다.
+    page = request.GET.get("page")  # Paginator 가 알아서 해주므로 dafault값 설정할 필요가 없어짐.
+    room_list = models.Room.objects.all()
+    paginator = Paginator(room_list, 10)
+    rooms = paginator.get_page(page)  # paginator 에서 page 불러오기위함
+    # print(vars(rooms.paginator)) 해보면 Queryset 안에 count, num_pages, per_page등 수많은 기능(?) 이 존재함을 볼 수 있다.
+
+    # render 의 인자를 살펴보면, request 가 필요한데, request 없이 response 는 있을 수 없음. 그래서 뭔가를 render 하고 싶다면, request를 주어야 함.(render(request, ... ))
+    return render(request, "rooms/home.html", {"rooms": rooms})
 
     # return HttpResponse(content = "hi~")
