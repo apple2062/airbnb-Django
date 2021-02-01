bfd896efb6a5b24a629c9624030e19f083059d97
yeonju <apple2062@naver.com>
2021-02-02 04:48:30 +0900

    adding as_view() method..

--- rooms/urls.py
+++ rooms/urls.py
@@ -8,6 +8,6 @@ app_name = "rooms"
 
 urlpatterns = [
     path(  # 이 path도 선언 후 config 안의 url파일 내에 import 해주어야만 한다.
-        "<int:pk>", views.RoomDetail, name="detail"
+        "<int:pk>", views.RoomDetail.as_view(), name="detail"
     )
 ]
