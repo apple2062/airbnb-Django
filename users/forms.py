3c48efc5650f2d1de7f3c574de2b16e0e3d086bc
yeonju <apple2062@naver.com>
2021-02-14 18:48:44 +0900

    email과 password 두 코드 상 반복이 많음 (-> will be modified)

--- users/forms.py
+++ users/forms.py
@@ -12,14 +12,26 @@ class LoginForm(forms.Form):
     # clean_ 으로 시작되는 method 들이 하는 일은 에러를 넣는 것 뿐만아니라 데이터를 정리도 해줌
     # 내가 만약 clean method 를 하나 만들었다면 , 근데 내가 아무것도 return 하지 않으면 그 field를 지워버린다. 따라서 return 해주어야해!
     # return 을 하면 field가 아직 유효하기에, cleaned_data 라는 메소드 안에서 field 를 볼 수 있다.
+    # clean_data 는 정리의 결과물이라고 말할 수 있음
     def clean_email(self):
         email = self.cleaned_data.get("email")
         # 유저를 가지고 왔는데 작동되지 않는다면, 유저가 없다는 뜻
         try:
+            # username 은 email과 같에 할 것이기 때문에 아래와 같이 작성
             models.User.objects.get(username=email)
             return email
         except models.User.DoesNotExist:
             raise forms.ValidationError("User dose not exist!")
 
     def clean_password(self):
-        return "blah~~~"
+        email = self.cleaned_data.get("email")
+        password = self.cleaned_data.get("password")
+        try:
+            user = models.User.objects.get(username=email)
+            # check_password 메소드 ? 패스워드를 확인하기 위해서는 user의 정보가 필요할 것이다. 때문에 이 메소드가 필요하고, 결국 이 메소드를 사용하려면 user에 붙여서 사용해야함
+            if user.check_password(password):
+                return password
+            else:
+                raise forms.ValidationError("Password is wrong")
+        except models.User.DoesNotExist:
+            pass
